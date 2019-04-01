69f8e3eff614c89a9e4635d08d1201245888f723
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2019-04-01 16:46:30 -0600

    rename to the website name

---
+++ algoDaily/reverseLinkedList.js
@@ -0,0 +1,11 @@
+class LinkedListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+function reverseList(head) {
+	if (head.length < 2) {
+		return;
+  }
+}
