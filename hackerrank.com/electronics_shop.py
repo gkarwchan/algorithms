bdcd3abe21c4828364f2f85c6b0be085bbce66b9
gkarwchan <ghassan.karwchan@gmail.com>
2019-04-13 23:43:31 -0600

    modify the code

--- hackerrank.com/electronics_shop.py
+++ hackerrank.com/electronics_shop.py
@@ -1,10 +1,17 @@
+'''
+Pythonic way: one line
+But it is slow: O(n^2)
+'''
+def getMoneySpent(keyboards, drives, b):
+  return max([k + d for k in keyboards for d in drives if b >= k + d], default = -1)
 
 
-'''First attempt: clean.
+'''
+Faster: O(n)
 we sort by descending, so there is a probability that it can be faster as we seek the higher to spend
 '''
 
-def getMoneySpent(keyboards, drives, b):
+def getMoneySpent2(keyboards, drives, b):
   keyboards.sort(reverse = True)
   drives.sort(reverse = True)
   amount = -1
@@ -24,11 +31,13 @@ The same as above, but in case there is no enough money,
 no need to go through all the loop, and exit immediateley 
 '''
 
-def getMoneySpent2(keyboards, drives, b):
+def getMoneySpent3(keyboards, drives, b):
   keyboards.sort(reverse = True)
   drives.sort(reverse = True)
   if keyboards[-1] + drives[-1] > b:
     return -1
+  if keyboards[0] + drives[0] <= b:
+    return keyboards[0] + drives[0]
   amount = 0
   for drive in drives:
     for keyboard in keyboards:
--- hackerrank.com/sandbox.py
+++ hackerrank.com/sandbox.py
@@ -1,11 +1,31 @@
-for x in range(7):
-  print ('---------------------------')
-  for y in range(7):
-    print ('  *********************')
-    for z in range(7):
-      print (x, y, z)
-      if z == 3:
-        break
-    print ('  *********************')
-  print ('---------------------------')
+import os
+import sys
+
+
+def getMoneySpent(keyboards, drives, b):
+  return max([k + d for k in keyboards for d in drives if b >= k + d], default = -1)
   
+
+if __name__ == '__main__':
+    
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    print(str(moneySpent))
+
