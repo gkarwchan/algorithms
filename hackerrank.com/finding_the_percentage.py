916b9b08d3291dded1ab5f206e4247c0e38eaa9e
gkarwchan <ghassan.karwchan@gmail.com>
2019-04-14 23:10:36 -0600

    finding the percentage

---
+++ hackerrank.com/finding_the_percentage.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+  dictionary = {}
+  for _ in range(int(input())):
+    name, *marks = input().split()
+    dictionary[name] = list(map(float, marks))
+  print("{:.2f}".format(sum(dictionary[input()]) / 3.0))
