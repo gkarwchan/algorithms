5d85da1c44759c730d02432c8620117934e0a679
gkarwchan <ghassan.karwchan@gmail.com>
2019-04-18 23:26:15 -0600

    add climbing the leader

---
+++ hackerrank.com/problem-solving/climbing_the_lead_board.py
@@ -0,0 +1,22 @@
+def climbingLeaderboard(scores, alice):
+    ranks = sorted(list(set(scores)), reverse=True)
+    aliceRanks = []
+    maxRight = len(ranks) - 1
+    for aliceScore in alice:
+        left = 0
+        right = maxRight
+        while right - left > 1:
+            mid = (right - left) // 2
+            if aliceScore > ranks[mid + left]:
+              # move left
+              right = mid + left
+            else:
+              left = mid + left
+        if aliceScore >= ranks[left]:
+          aliceRanks.append(left + 1)
+        elif aliceScore < ranks[right]:
+          aliceRanks.append(right + 2)
+        else:
+          aliceRanks.append(right + 1)
+          
+    return aliceRanks
