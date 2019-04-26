6212323964ced8e9cb4c2838d106bbee301b909d
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2019-04-26 05:09:00 -0600

    add alphabet rangoli

---
+++ hackerrank.com/python/alphabet_rangoli.py
@@ -0,0 +1,27 @@
+'''
+without importing ascii
+'''
+
+
+def print_rangoli(size):
+    chars = [chr(i + ord('a')) for i in range (size -1, -1, -1)]
+    output = ['-'.join(chars[:i] + chars[i::-1]).center(4 * size - 3, '-') for i in range(size)]
+    print ('\n'.join(output + output[-2::-1]))
+
+
+
+
+'''
+with importing ascii
+'''
+import string
+
+def print_rangoli2(size):
+    chars = string.ascii_lowercase
+    output = ['-'.join(chars[:i] + chars[i::-1]).center(4 * size - 3, '-') for i in range(size)]
+    print ('\n'.join(output + output[-2::-1]))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
