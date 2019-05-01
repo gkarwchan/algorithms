c9143c1cbea37bef93220d0636ef973868b4f3a9
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2019-04-30 21:01:29 -0600

    add circular array

---
+++ hackerrank.com/problem-solving/circular_array_rotation.py
@@ -0,0 +1,9 @@
+def circularArrayRotation(a, k, queries):
+    return [a[(val - k) % len(a)] for val in queries]
+
+if __name__ == '__main__':
+    n, k, q = map(int, input().split())
+    arr = list(map(int, input().split()))
+    queries = [int(input()) for _ in range(q)]
+    result = circularArrayRotation(arr, k, queries)
+    print('\n'.join(map(str, result)))
