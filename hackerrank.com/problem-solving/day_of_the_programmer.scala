ed9088d4fde97c8e00bfa8c67dd978ada22f0db7
gkarwchan <ghassan.karwchan@gmail.com>
2019-05-26 22:42:26 -0600

    add more scala

--- hackerrank.com/problem-solving/day_of_the_programmer.scala
+++ hackerrank.com/problem-solving/day_of_the_programmer.scala
@@ -15,6 +15,16 @@ object Solution {
     return "13.09.%d".format(year)
   }
 
+  def dayOfProgrammer1(year: Int): String = {
+    val leapYearDate = s"12.09.$year"
+    val nonLeapYearDate = s"13.09.$year"
+
+    year match {
+      case 1918 => "26.09.1918"
+      case _ if year < 1918 => if (year % 4 == 0) leapYearDate else nonLeapYearDate
+      case _ => if (year % 400 == 0 || (year % 4 == 0 && !(year % 100 == 0))) leapYearDate else nonLeapYearDate
+    }
+  }
 
   def main(args: Array[String]) {
     val year = readLine.trim.toInt
