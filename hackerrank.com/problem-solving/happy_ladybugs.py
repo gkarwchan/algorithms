21f2bc194ed5af2574e65b382d7fb1ab27a2d826
gkarwchan <ghassan.karwchan@gmail.com>
2019-07-28 12:01:31 -0600

    add more options for lady bugs

--- hackerrank.com/problem-solving/happy_ladybugs.py
+++ hackerrank.com/problem-solving/happy_ladybugs.py
@@ -1,10 +1,14 @@
 # https://www.hackerrank.com/challenges/happy-ladybugs/problem
 
+
+''' 
+Without using any extra library
+'''
+
 def happyLadybugs(b):
   count = { i:b.count(i) for i in set(b) }
-  for k, v in count.items():
-    if v == 1 and k != '_':
-      return 'NO'
+  if any(count[k] == 1 for k in count if k != '_'):
+    return 'NO'
   if '_' in b:
     return 'YES'
   lastChar = None
@@ -18,6 +22,18 @@ def happyLadybugs(b):
     else:
       isSingle = False
   return 'NO' if isSingle else 'YES'
+
+'''
+with using collections.Counter which is enhanced for performance
+'''
+
+from collections import Counter
+
+def happyLadybugs1(b):
+  count = Counter(b)
+  if any(count[k] == 1 for k in count if k != '_'):
+      return 'NO'
+  return 'NO' if '_' not in b and any (b[i] != b[i - 1] and b[i] != b[i + 1] for i in range(1, n - 1)) else 'YES'
 
 
 if __name__ == '__main__':
