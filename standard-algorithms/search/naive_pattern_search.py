4fac3b5c77e2c1297a5dd9bb15969b6f65472538
gkarwchan <ghassan.karwchan@gmail.com>
2020-01-25 18:14:25 -0700

    add pattern search

--- standard-algorithms/search/naive_pattern_search.py
+++ standard-algorithms/search/naive_pattern_search.py
@@ -1,14 +1,25 @@
 def naive_pattern_search(pattern, text):
   founds = []
-  currentMatches = [0]
+  patternIndices = []
   for i in range(len(text)):
-    if text[i] == pattern[currentPatternIndex]:
-      currentPatternIndex += 1
-      if currentPatternIndex == len(pattern):
-        founds.append(i - len(pattern) + 1)
-        currentPatternIndex = 0
-    else:
-      currentPatternIndex = 0
+    j = 0
+    while j < len(patternIndices):
+      if text[i] == pattern[patternIndices[j]]:
+        patternIndices[j] += 1
+        if patternIndices[j] == len(pattern):
+          founds.append(i - len(pattern) + 1)
+          del patternIndices[j]
+        else:
+          j += 1
+      else:
+        del patternIndices[j]
+    if text[i] == pattern[0]:
+      if len(pattern) == 1:
+        founds.append(i)
+      else:
+        patternIndices.append(1)
+    
+      
   return founds
 
 
